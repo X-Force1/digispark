9750457f0997455c2ddfb62abc6e72eb0ea80b07
X-Force1 <49411153+X-Force1@users.noreply.github.com>
2019-04-09 13:59:59 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,7 +1,8 @@
-$ie = New-Object -ComObject InternetExplorer.Application
-$ie.Navigate("http://fakeupdate.net/win10u/")
-$ie.Visible = $true
-$ie.fullscreen = $true;
+start microsoft-edge:http://fakeupdate.net/win10u/
+$wshell = New-Object -ComObject wscript.shell
+$wshell.AppActivate('Google - Microsoft Edge')
+Sleep 2
+$wshell.SendKeys('{F11}')
 
 #Clear Run
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
