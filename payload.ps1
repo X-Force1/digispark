39ba6b2c94ada36a7b23e47f47c4a0bc84a9c3f1
X-Force1 <49411153+X-Force1@users.noreply.github.com>
2019-04-09 13:49:27 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,13 +1,7 @@
 $ie = New-Object -ComObject InternetExplorer.Application
 $ie.Navigate("http://fakeupdate.net/win10u/")
 $ie.Visible = $true
-$sw = @'
-[DllImport("user32.dll")]
-public static extern int ShowWindow(int hwnd, int nCmdShow);
-'@
-
-$type = Add-Type -Name ShowWindow2 -MemberDefinition $sw -Language CSharpVersion3 -Namespace Utils -PassThru
-$type::ShowWindow($ie.hwnd, 3) # 3 = maximize 
+$ie.fullscreen = $true;
 
 #Clear Run
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
